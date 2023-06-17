9ea45ae78525bf2c7ac8259ea7dfce2d42cb33f2
Matt Schiffman <>
2023-06-16 21:03:55 -0400

    added filter to users

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,7 +1,9 @@
 import {IconCodeFork} from "./icon-code-fork";
+import {IconFilter} from "./icon-filter";
 import {IconStar} from "./icon-star";
 
 export {
   IconCodeFork,
+  IconFilter,
   IconStar
 }
--- src/components/users/users.tsx
+++ src/components/users/users.tsx
@@ -1,8 +1,10 @@
-import React, {useContext, useEffect} from "react";
+import React, {ChangeEvent, useContext, useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 
 import {ServiceContext} from "../../core/service-context";
+
+import {IconFilter} from "../icons";
 
 import {IInitialState} from "../../core/store";
 import {IUser} from "../../core/schemas";
@@ -16,6 +18,14 @@ const Users = () => {
   const users: IUser[] = useSelector((state: IInitialState) => state.users.users);
   const selectedUser = useSelector((state: IInitialState) => state.userDetails.user);
 
+  const [filterValue, setFilterValue] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState(users);
+
+  const onFilterChange = (e:ChangeEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+    setFilterValue(value);
+  }
+
   useEffect(() => {
     if (users.length === 0) {
       dispatch(usersAction.asyncRequestUsers());
@@ -25,10 +35,27 @@ const Users = () => {
     }
   }, []);
 
+  useEffect(() => {
+    setFilteredUsers(users);
+  }, [users]);
+
+  useEffect(() => {
+    const filtered = users.filter((user) => {
+      return user.login.toLowerCase().includes(filterValue.toLowerCase());
+    });
+    setFilteredUsers(filtered);
+  }, [filterValue]);
+
   return (
-    <div className="home">
+    <div className="users">
+      <div className="filter-users">
+        <div className="filter-icon">
+          <IconFilter color="#EFEEEE" height="28" width="28" />
+        </div>
+        <input value={filterValue} onChange={onFilterChange} />
+      </div>
       <ul>
-        {users.map((user) => {
+        {filteredUsers.map((user) => {
           return (
             <li key={user.node_id}>
               <Link to={`/${user.login}`}>
