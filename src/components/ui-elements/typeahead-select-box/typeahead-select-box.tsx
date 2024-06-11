f9e44288bb2fb2854dd85fb7761433b6edd21c86
Matthew Schiffman <matt@darrownet.com>
2024-06-10 22:08:15 -0400

    completed typeahead

--- src/components/ui-elements/typeahead-select-box/typeahead-select-box.tsx
+++ src/components/ui-elements/typeahead-select-box/typeahead-select-box.tsx
@@ -1,6 +1,7 @@
-import { FC, FormEvent, useState } from 'react';
+import { FC, MouseEvent, FormEvent, useState } from 'react';
 import {IPost} from "../../../core/schemas";
 import {Link} from "react-router-dom";
+import {FALSE} from "sass";
 
 interface ITypeaheadSelectBox {
   options: IPost[];
@@ -11,10 +12,12 @@ const TypeaheadSelectBox:FC<ITypeaheadSelectBox> = ({ options }) => {
   const [filteredOptions, setFilteredOptions] = useState<IPost[]>([]);
   const [, setSelectedOption] = useState<IPost | null>(null);
   const [showOptions, setShowOptions] = useState(false);
+  const [showAllPosts, setShowAllPosts] = useState(false);
 
-  const handleInputChange = (event:FormEvent<HTMLInputElement>) => {
+  const onInputChange = (event:FormEvent<HTMLInputElement>) => {
     const value = event.currentTarget.value;
     setInputValue(value);
+    setShowAllPosts(false);
     if (value.length > 0) {
       const filtered = options.filter(option =>
           option.title.toLowerCase().includes(value.toLowerCase())
@@ -26,24 +29,42 @@ const TypeaheadSelectBox:FC<ITypeaheadSelectBox> = ({ options }) => {
     }
   };
 
-  const handleOptionClick = (option:IPost) => {
+  const onOptionClick = (option:IPost) => {
     setInputValue(option.title);
     setSelectedOption(option);
     setShowOptions(false);
   };
 
+  const onSelectClick = () => {
+    setShowAllPosts(!showAllPosts);
+    setFilteredOptions([]);
+    setInputValue('');
+  };
+
   return (
       <div className="typeahead-select-box">
-        <input
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            placeholder="select a post or search here.."
-        />
+        <div className="input-field">
+          <input
+              type="text"
+              value={inputValue}
+              onChange={onInputChange}
+              placeholder="select a post or search here.."
+          />
+          <i className={`select-aff${showAllPosts ? ' open' : ' closed'}`} onClick={onSelectClick}></i>
+        </div>
         {showOptions && (
             <ul className="options-list">
               {filteredOptions.map((option, index) => (
-                  <li key={index} onClick={() => handleOptionClick(option)}>
+                  <li key={index} onClick={() => onOptionClick(option)}>
+                  <Link to={`/posts/${option.id}`}>{option.title}</Link>
+                  </li>
+              ))}
+            </ul>
+        )}
+        {showAllPosts && (
+            <ul className="options-list">
+              {options.map((option, index) => (
+                  <li key={index} onClick={() => onOptionClick(option)}>
                     <Link to={`/posts/${option.id}`}>{option.title}</Link>
                   </li>
               ))}
