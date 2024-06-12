e8473e5a967d4c69a874309e23406e71fb47c475
Matthew Schiffman <matt@darrownet.com>
2024-06-12 13:59:14 -0400

    local form functionality complete

--- src/components/post-details/post-details.tsx
+++ src/components/post-details/post-details.tsx
@@ -1,6 +1,5 @@
-import React, {useContext, useEffect, useState} from "react";
+import {ChangeEvent, FormEvent, useContext, useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {useNavigate} from "react-router-dom";
 import {useParams} from 'react-router-dom';
 
 import {ServiceContext} from "../../core/service-context";
@@ -16,9 +15,28 @@ const PostDetails = () => {
   const selectedPost = useSelector((state: IInitialState) => state.postDetails.post);
   const selectedPostComments = useSelector((state: IInitialState) => state.postDetails.comments);
 
-  const navigate = useNavigate();
   const params = useParams();
   const [postId] = useState(params.postId);
+
+  const [newName, setNewName] = useState('');
+  const [newEmail, setNewEmail] = useState('');
+  const [newComment, setNewComment] = useState('');
+
+  const onFieldChange = (event:ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>, fieldName: 'name' | 'email' | 'comment') => {
+    const fieldValue = event.currentTarget.value;
+    type FieldMapper = {name: Function, email: Function, comment: Function}
+    const fieldMapper:FieldMapper = {
+      name: () => setNewName(fieldValue),
+      email: () => setNewEmail(fieldValue),
+      comment: () => setNewComment(fieldValue)
+    }
+    fieldMapper[fieldName]();
+  }
+
+  const onFormSubmit = (event:FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(newName, newEmail, newComment);
+  }
 
   useEffect(() => {
     dispatch(actions.asyncPostDataRequest({actionTypeValue: 'post', route: `/posts/${postId}`}));
@@ -41,18 +59,18 @@ const PostDetails = () => {
             );
           })}
         </ul>
-        <form>
+        <form onSubmit={onFormSubmit}>
           <div className="label-input">
             <label htmlFor="name">Name:</label>
-            <input name="name" type="test"/>
+            <input name="name" type="test" onChange={(event) => onFieldChange(event, 'name')}/>
           </div>
           <div className="label-input">
             <label htmlFor="email">Email:</label>
-            <input name="email" type="email"/>
+            <input name="email" type="email" onChange={(event) => onFieldChange(event, 'email')}/>
           </div>
           <div className="label-textarea">
             <label htmlFor="comment">Comment:</label>
-            <textarea name="comment"></textarea>
+            <textarea name="comment" onChange={(event) => onFieldChange(event, 'comment')}></textarea>
           </div>
           <div className="submit">
             <input name="email" type="submit" value="post"/>
